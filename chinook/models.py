fcef079ba11dd726ceab5b10b7224e63d1bacaa5
cudjoeab <cudjoeab@gmail.com>
2019-08-05 13:22:35 -0400

    added many-many field to models

--- chinook/models.py
+++ chinook/models.py
@@ -12,16 +12,22 @@ class Album(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genres = models.ManyToManyField("Genre", through= "Track")
+    media_types = models.ManyToManyField("MediaType", through="Track")
 
 class MediaType(models.Model):
     name = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    genres = models.ManyToManyField("Genre", through="Track")
+    albums = models.ManyToManyField("Album", through="Track")
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    media_types = models.ManyToManyField("MediaType", through="Track")
+    albums = models.ManyToManyField("Album", through="Track")
 
 class Track(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
